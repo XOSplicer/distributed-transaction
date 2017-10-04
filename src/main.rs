ab4bc8028593853013911dac579316b0504a1b46
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-10-04 17:56:51 +0200

    add board unit test

--- src/main.rs
+++ src/main.rs
@@ -196,3 +196,40 @@ fn main() {
     transactions.push(t4);
 
 }
+
+mod test {
+
+    use super::*;
+    use chrono::prelude::*;
+
+    #[test]
+    fn board_example() {
+        let mut transactions = Vec::new();
+
+        let e1 = "00000001;041017-10:00:00;00;01;Testü;267C4D5033ED7F96B43216FD8C871E4B96F1221204312AD6F43362F2D12C9B29\n";
+        let t1 = Transaction::build()
+            .with_id(1)
+            .with_timestamp(DateTime::parse_from_rfc3339("2017-10-04T10:00:00+01:00").unwrap())
+            .with_group_id(0)
+            .with_process_id(1)
+            .with_text("Testü".to_owned())
+            .try_finish(&transactions)
+            .unwrap();
+        let s1 = t1.to_string();
+        assert_eq!(e1, s1);
+        transactions.push(t1);
+
+        let e2 = "00000002;041017-10:01:00;00;01;Test2;6CB09B876CA855D7F3D8168E2001594E354D2EFEC110F5D5FCED478641E96C9C\n";
+        let t2 = Transaction::build()
+            .with_id(2)
+            .with_timestamp(DateTime::parse_from_rfc3339("2017-10-04T10:01:00+01:00").unwrap())
+            .with_group_id(0)
+            .with_process_id(1)
+            .with_text("Test2".to_owned())
+            .try_finish(&transactions)
+            .unwrap();
+        let s2 = t2.to_string();
+        assert_eq!(e2, s2);
+        transactions.push(t2);
+    }
+}
