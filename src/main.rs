a38fe5772b4330101fc161671d1307c1ddda4c43
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-10-04 17:46:36 +0200

    use builder pattern

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 
-extern crate sha2;
 extern crate chrono;
 extern crate itertools;
+extern crate sha2;
 
 use itertools::Itertools;
 use chrono::prelude::*;
-use sha2::{Sha256, Digest};
+use sha2::{Digest, Sha256};
 
 #[derive(Debug)]
 struct Transaction<Tz: TimeZone> {
@@ -17,38 +17,62 @@ struct Transaction<Tz: TimeZone> {
     checksum: Vec<u8>,
 }
 
+struct TransactionBuilder<Tz: TimeZone> {
+    id: Option<u32>,
+    timestamp: Option<DateTime<Tz>>,
+    group_id: Option<u8>,
+    process_id: Option<u8>,
+    text: Option<String>,
+}
+
 impl Transaction<chrono::FixedOffset> {
+
+    const MIN_ID: u32 = 1;
+    const MAX_ID: u32 = 99_999_999;
+    const MIN_GID: u8 = 0;
+    const MAX_GID: u8 = 99;
+    const MIN_PID: u8 = 0;
+    const MAX_PID: u8 = 99;
+    const INVALID_CHAR: &'static[&'static str] = &["\n", "\r", "\t", ";", "\0"];
+
+    pub fn build() -> TransactionBuilder<chrono::FixedOffset> {
+        TransactionBuilder::new()
+    }
+
     pub fn new(
         prev: &Vec<Transaction<chrono::FixedOffset>>,
         id: u32,
         time: DateTime<chrono::FixedOffset>,
         gid: u8,
         pid: u8,
-        text: &str,
+        text: String,
     ) -> Result<Transaction<chrono::FixedOffset>, String> {
 
-        let max_gid = 99;
-        let max_pid = 99;
-        let max_id = 99_999_999;
-
-        if gid > max_gid {
+        // Check input parameters
+        if id < Transaction::MIN_ID || id > Transaction::MAX_ID {
+            return Err(format!("Invalid id {}", id));
+        }
+        if gid < Transaction::MIN_GID || gid > Transaction::MAX_GID {
             return Err(format!("Invalid group id {}", gid));
         }
-        if pid > max_pid {
+        if pid < Transaction::MIN_PID || pid > Transaction::MAX_PID {
             return Err(format!("Invalid process id {}", pid));
         }
+        if Transaction::INVALID_CHAR.iter().any(|c| text.contains(c)) {
+            return Err(format!("Invalid text message `{}`", text));
+        }
 
-        let msg = text.replace("\n", "").replace("\r", "").replace(";", "");
-
+        // Hash all previous transaction strings
+        // and the partial new transaction
         let mut hasher = Sha256::default();
         for t in prev.iter() {
             hasher.input(t.to_string().into_bytes().as_ref());
         }
-
-
-        hasher.input(Self::partial_string(id, &time, gid, pid, &msg)
-            .into_bytes().as_ref());
-
+        hasher.input(
+            Self::partial_string(id, &time, gid, pid, &text)
+                .into_bytes()
+                .as_ref(),
+        );
         let checksum = Vec::from(hasher.result().as_slice());
 
         Ok(Transaction {
@@ -56,13 +80,20 @@ impl Transaction<chrono::FixedOffset> {
             timestamp: time,
             group_id: gid,
             process_id: pid,
-            text: msg,
+            text: text,
             checksum: checksum,
         })
     }
 
-    fn partial_string(id: u32, ts: &DateTime<chrono::FixedOffset>, gid: u8, pid: u8, text: &str) -> String {
-        format!("{id:08};{ts};{gid:02};{pid:02};{msg};",
+    fn partial_string(
+        id: u32,
+        ts: &DateTime<chrono::FixedOffset>,
+        gid: u8,
+        pid: u8,
+        text: &str,
+    ) -> String {
+        format!(
+            "{id:08};{ts};{gid:02};{pid:02};{msg};",
             id = id,
             ts = ts.format("%d%m%y-%H:%M:%S"),
             gid = gid,
@@ -72,37 +103,96 @@ impl Transaction<chrono::FixedOffset> {
     }
 
     pub fn to_string(&self) -> String {
-        format!("{partial}{chksm:02X}\n",
-            partial = Self::partial_string(self.id, &self.timestamp, self.group_id, self.process_id, &self.text),
+        format!(
+            "{partial}{chksm:02X}\n",
+            partial = Self::partial_string(
+                self.id,
+                &self.timestamp,
+                self.group_id,
+                self.process_id,
+                &self.text
+            ),
             chksm = self.checksum.iter().format(""),
         )
     }
+}
 
+impl TransactionBuilder<chrono::FixedOffset> {
+    pub fn new() -> Self {
+        TransactionBuilder {
+            id: None,
+            timestamp: None,
+            group_id: None,
+            process_id: None,
+            text: None,
+        }
+    }
+
+    pub fn with_id(mut self, id: u32) -> Self {
+        self.id = Some(id);
+        self
+    }
+
+    pub fn with_timestamp(mut self, ts: DateTime<chrono::FixedOffset>) -> Self {
+        self.timestamp = Some(ts);
+        self
+    }
+
+    pub fn with_group_id(mut self, gid: u8) -> Self {
+        self.group_id = Some(gid);
+        self
+    }
+
+    pub fn with_process_id(mut self, pid: u8) -> Self {
+        self.process_id = Some(pid);
+        self
+    }
+
+    pub fn with_text(mut self, text: String) -> Self {
+        self.text = Some(text);
+        self
+    }
+
+
+    pub fn try_finish(
+        self,
+        prev: &Vec<Transaction<chrono::FixedOffset>>,
+    ) -> Result<Transaction<chrono::FixedOffset>, String> {
+        Transaction::new(
+            prev,
+            self.id.ok_or("No id given".to_string())?,
+            self.timestamp.ok_or("No timestamp given".to_string())?,
+            self.group_id.ok_or("No group id given".to_string())?,
+            self.process_id.ok_or("No process id given".to_string())?,
+            self.text.unwrap_or("".to_string())
+        )
+    }
 }
 
 
 fn main() {
     let mut transactions = Vec::new();
 
-    let t1 = Transaction::new(
-        &vec![],
-        1,
-        DateTime::parse_from_rfc3339("2017-10-04T10:00:00+01:00").unwrap(),
-        0,
-        1,
-        "Testü"
-        ).unwrap();
-    println!("{:#?}", t1.to_string());
-    transactions.push(t1);
+    let t3 = Transaction::build()
+        .with_id(1)
+        .with_timestamp(DateTime::parse_from_rfc3339("2017-10-04T10:00:00+01:00").unwrap())
+        .with_group_id(0)
+        .with_process_id(1)
+        .with_text("Testü".to_owned())
+        .try_finish(&transactions)
+        .unwrap();
+    println!("{:#?}", t3.to_string());
+    transactions.push(t3);
 
-    let t2 = Transaction::new(
-        &transactions,
-        2,
-        DateTime::parse_from_rfc3339("2017-10-04T10:01:00+01:00").unwrap(),
-        0,
-        1,
-        "Test2"
-        ).unwrap();
+    let t4 = Transaction::build()
+        .with_id(2)
+        .with_timestamp(DateTime::parse_from_rfc3339("2017-10-04T10:01:00+01:00").unwrap())
+        .with_group_id(0)
+        .with_process_id(1)
+        .with_text("Test2".to_owned())
+        .try_finish(&transactions)
+        .unwrap();
+    println!("{:#?}", t4.to_string());
+    transactions.push(t4);
 
-    println!("{:#?}", t2.to_string());
 }
