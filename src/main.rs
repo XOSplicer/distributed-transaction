2e47e0f67066ed891c0677a51db136b89d09c016
Felix Stegmaier <stegmaier.felix@gmail.com>
2017-10-19 01:41:10 +0200

    basic clap

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,37 @@ extern crate chrono;
 extern crate itertools;
 extern crate sha2;
 #[macro_use] extern crate quick_error;
+extern crate clap;
 
 mod transaction;
 mod transaction_log;
+
+use clap::{App, Arg};
 
 use transaction::Transaction;
 use transaction_log::*;
 
 fn main() {
+    let arg_matches = App::new("transaction")
+        .arg(Arg::with_name("pid")
+            .long("pid")
+            .takes_value(true))
+        .arg(Arg::with_name("gid")
+            .long("gid")
+            .takes_value(true))
+        .get_matches();
+
     let n = 10;
     //let mut tx_log = FullTransactionLog::with_capactity(n);
     //let mut tx_log = DirectFileLog::new("/tmp/tx_log_tmp").unwrap();
     let mut tx_log = SyncroizedFileLog::new("/tmp/transaction_log.tmp");
     tx_log.syncronize().unwrap();
-    let gid = 0;
-    let pid = 1;
+    let gid = arg_matches.value_of("gid")
+        .and_then(|s| s.parse::<u8>().ok())
+        .unwrap_or(0);
+    let pid = arg_matches.value_of("pid")
+        .and_then(|s| s.parse::<u8>().ok())
+        .unwrap_or(0);
     for _ in 0..n {
         let tx = Transaction::build()
             .with_id(tx_log.next_id())
